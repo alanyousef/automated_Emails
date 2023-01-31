dfdaa8b42440744e35e811583b5da3b1be96fda2
alanyousef <103208852+alanyousef@users.noreply.github.com>
2023-01-31 12:08:36 -0500

    init

---
+++ main.js
@@ -0,0 +1,28 @@
+require('dotenv').config
+const nodemailer = require("nodemailer");
+
+
+
+(async function run(){
+console.log('running my daily report')
+
+let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: process.env.MAIL_USER_EMAIL, // generated ethereal user
+      pass: process.env.MAIL_USER_PASSWORD, // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+   await transporter.sendMail({
+    from: 'Automotion Reports <automotionreports@gmail.com>', // sender address
+    to: "alany@adgorg.com", // list of receivers
+    subject: "Daily Report", // Subject line
+    text: `Daily Report`, // plain text body
+    html: `<h1>Daily Report</h1>`, // html body
+  });
+
+})();
